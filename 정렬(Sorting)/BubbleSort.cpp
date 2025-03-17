c7f5001fdef2fe51a69788c9a5398b33bd7b3a14
Bela <1692074@hansung.ac.kr>
2025-03-17 15:21:59 +0900

    정렬(Sorting) 코드 추가

---
+++ 정렬(Sorting)/BubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    bool swapped;
+
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = { 5, 3, 8, 4, 2 };
+
+    bubbleSort(arr);
+
+    for (int num : arr) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
